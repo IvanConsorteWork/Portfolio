a41e4e48ce8ba2b8cfa4a70f275fb6276ffb9773
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2023-07-13 00:35:12 -0300

    adding old hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,23 @@
-import './Hero.css'
+import React from 'react';
+import introImg from "../assets/intro-bg.jpg";
 import ProfilePicture from '../assets/FOTOPERFILLINKEDIN.jpg'
 
-export default function Hero () {
+import './Hero.css';
+
+export default function Hero() {
   return (
-    <div id="hero">
-      <div id='hero-img'>
-        <img src={ProfilePicture} alt='profile-picture' loading='lazy'/>
+    <div id='home-hero'>
+      <div id='home-mask'>
+          <img src={introImg} alt="background"/>
       </div>
-      <div id='hero-text'>
-        <p>Hi! I'm Ivan Consorte, a <span>FRONT-END REACT DEVELOPER</span> specialiced in creating highly functional, scalable, and complete web experiences. With a meticulous attention to detail, I ensure every aspect of your website is flawlessly executed. Let's create exceptional digital experiences together!</p>
+      <div id="home-content">
+        <div id='home-text-container'>
+          <p>Hi! I'm Ivan Consorte, a <span>FRONT-END REACT DEVELOPER </span>specialiced in creating highly functional, scalable, and complete web experiences. With a meticulous attention to detail, I ensure every aspect of your website is flawlessly executed. Let's create exceptional digital experiences together!</p>
+        </div>
+        <div id='home-img-container'>
+          <img src={ProfilePicture} alt='profile-picture' loading='lazy'/>
+        </div>
       </div>
-  </div>
+    </div>
   )
 }
